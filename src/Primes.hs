b093f55c913b12a4dc10894a9d06c1139376ea08
residenti <n.queue.r@gmail.com>
2022-05-08 16:31:35 +0900

    Eratosthenes' sieve

--- src/Primes.hs
+++ src/Primes.hs
@@ -2,3 +2,8 @@ module Primes where
 
 primes :: [Int]
 primes = [1 ..]
+
+sieve :: [Int] -> [Int]
+sieve [] = []
+sieve (nextPrime:rest) = nextPrime : sieve noFactors
+  where noFactors = filter (not . (==0) . (`mod` nextPrime)) rest
