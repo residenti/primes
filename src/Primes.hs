39e26860cea01e9f47810d1da9a1be8d040da107
residenti <n.queue.r@gmail.com>
2022-05-10 23:28:56 +0900

    Define prime numbers.

--- src/Primes.hs
+++ src/Primes.hs
@@ -1,9 +1,11 @@
 module Primes where
 
-primes :: [Int]
-primes = [1 ..]
-
 sieve :: [Int] -> [Int]
 sieve [] = []
 sieve (nextPrime:rest) = nextPrime : sieve noFactors
   where noFactors = filter (not . (==0) . (`mod` nextPrime)) rest
+
+-- maxBound :: Int -> 9223372036854775807
+-- Limit the maximum value to 10000.
+primes :: [Int]
+primes = sieve [2 .. 10000]
